04a7746e00e40113df483dabc92bb6693b5f6689
RokaPls <83350641+RokaPls@users.noreply.github.com>
2021-05-04 18:27:44 +0200

    new code

---
+++ Portal stuff/Assets/Script/First Try for portal/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    
+    public bool FirstPortal = true;
+
+    //portalmanager
+    public PortalManager portalManager;
+
+    [SerializeField]
+    private string hitTag;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != hitTag)
+            return;
+
+        //portal to other object
+        portalManager.PortalObject(FirstPortal, other.gameObject);
+    }
+}
